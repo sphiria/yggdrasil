182fcf8fb67247fdc1d00a1074b130e5274f0b92
gyaru <eryla@eru.me>
2016-11-11 23:48:06 +0100

    add wikimedia extensions

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,21 +8,24 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password_again passwo
 # update the box
 apt-get update
 
+# set-up sync folder for nginx before
+ln -fs /synced/nginx /etc/nginx
+
 # set-up nginx
 apt-get install -y nginx-full
 systemctl stop nginx.service # we don't need it right now
 
 # copy nginx config + site config
 if ! [ -L /etc/nginx/nginx.conf ]; then
-  rm -rf /etc/nginx/nginx.conf
-  cp /vagrant/src/nginx/nginx.conf /etc/nginx/nginx.conf
-  cp /vagrant/src/nginx/mediawiki.conf /etc/nginx/mediawiki.conf
+	rm -rf /etc/nginx/nginx.conf
+	cp /vagrant/src/nginx/nginx.conf /etc/nginx/nginx.conf
+	cp /vagrant/src/nginx/mediawiki.conf /etc/nginx/mediawiki.conf
 fi
 
 # expose "interesting" folders to share
 if ! [ -L /srv/http/mediawiki ]; then
-  rm -rf /srv/http/mediawiki
-  ln -fs /synced/mediawiki /srv/http/mediawiki
+	rm -rf /srv/http/mediawiki
+	ln -fs /synced/mediawiki /srv/http/mediawiki
 fi
 
 # php5 for lazy debian defaults
@@ -103,6 +106,41 @@ wget "https://releases.wikimedia.org/mediawiki/1.27/mediawiki-1.27.1.tar.gz"
 tar xvf "mediawiki-1.27.1.tar.gz" -C /srv/http/mediawiki --strip-components=1
 
 # install mediawiki extensions
+
+# RandomSelection
+wget https://github.com/inclumedia/RandomSelection/archive/master.zip -O RandomSelection.zip
+unzip RandomSelection.zip -d /srv/http/mediawiki/extensions/
+mv /srv/http/mediawiki/extensions/RandomSelection-master/ /srv/http/mediawiki/extensions/RandomSelection/
+
+# MassEditRegex
+wget https://extdist.wmflabs.org/dist/extensions/MassEditRegex-REL1_27-ad743a7.tar.gz
+tar -xzf MassEditRegex-REL1_27-ad743a7.tar.gz -C /srv/http/mediawiki/extensions/
+
+# Variables
+wget https://extdist.wmflabs.org/dist/extensions/Variables-REL1_27-b0e1772.tar.gz
+tar -xzf Variables-REL1_27-b0e1772.tar.gz -C /srv/http/mediawiki/extensions/
+
+# Loops
+wget https://extdist.wmflabs.org/dist/extensions/Loops-REL1_27-19f9cc1.tar.gz
+tar -xzf Loops-REL1_27-19f9cc1.tar.gz -C /srv/http/mediawiki/extensions/
+
+# DynamicPageList3
+wget https://github.com/Alexia/DynamicPageList/archive/master.zip -O DynamicPageList3.zip
+unzip DynamicPageList3.zip -d /srv/http/mediawiki/extensions/
+mv /srv/http/mediawiki/extensions/DynamicPageList-master/ /srv/http/mediawiki/extensions/DynamicPageList/
+
+# Tabber
+wget https://github.com/HydraWiki/Tabber/archive/v2.4.zip -O Tabber.zip
+unzip Tabber.zip -d /srv/http/mediawiki/extensions/
+mv /srv/http/mediawiki/extensions/Tabber-2.4/ /srv/http/mediawiki/extensions/Tabber/
+
+# Arrays
+wget https://extdist.wmflabs.org/dist/extensions/Arrays-REL1_27-ead7f94.tar.gz
+tar -xzf Arrays-REL1_27-ead7f94.tar.gz -C /srv/http/mediawiki/extensions/
+
+# MsUpload
+wget https://extdist.wmflabs.org/dist/extensions/MsUpload-REL1_27-9415674.tar.gz
+tar -xzf MsUpload-REL1_27-9415674.tar.gz -C /srv/http/mediawiki/extensions/
 
 # restore mediawiki backups if they exist
 # LocalSettings.php
